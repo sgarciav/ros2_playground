39b3f7b68a964f3cda7218f192b63f478d7c4c47
Sergio Garcia-Vergara <sergiodotgarcia@gmail.com>
2023-05-22 06:48:22 -0400

    working launch file

--- ros2_ws/src/my_behavior_tree_pkg/launch/behavior_tree_engine.launch.py
+++ ros2_ws/src/my_behavior_tree_pkg/launch/behavior_tree_engine.launch.py
@@ -1,15 +1,9 @@
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, RegisterEventHandler, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.conditions import LaunchConfigurationEquals
-from launch.event_handlers import OnProcessExit
-from launch.substitutions import Command, FindExecutable, LaunchConfiguration, PathJoinSubstitution, PythonExpression
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
-
-from rif_launch.utils import (load_yaml, get_robot_description_semantic,
-                              get_robot_description_kinematics)
 
 
 def generate_launch_description():
@@ -28,12 +22,12 @@ def generate_launch_description():
     tree_filename = LaunchConfiguration("tree_filename")
 
     path_to_tree_file = PathJoinSubstitution(
-        [FindPackageShare("my_behavior_tree"), "behavior_trees", tree_filename]
+        [FindPackageShare("my_behavior_tree_pkg"), "behavior_trees", tree_filename]
     )
 
 
     beahvior_tree_engine_node = Node(
-        package="my_behavior_tree",
+        package="my_behavior_tree_pkg",
         executable="behavior_tree_engine",
         output={
             "stdout": "screen",
