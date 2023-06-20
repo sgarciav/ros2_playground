ef876bc311e8e58bbde339f752b9e90eed0971bd
Sergio Garcia Vergara <sergiodotgarcia@gmail.com>
2023-06-19 23:58:30 -0400

    deleted unnecessary code

--- ros2_ws/src/my_behavior_tree_pkg/src/ros_to_blackboard_btnode.cpp
+++
@@ -1,9 +0,0 @@
-#include "my_behavior_tree_pkg/ros_to_blackboard_btnode.hpp"
-
-// template <typename T>
-// BT::PortsList RosToBlackboard<T>::providedPorts()
-// {
-//   BT::PortsList list;
-//   list.insert({BT::OutputPort<T>("value")});
-//   return providedBasicPorts(list);
-// }
